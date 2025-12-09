4bb4f3a426de7ef6c3a9ddcc83991795c07646cc
Clarity <claridadjulyfranz@gmail.com>
2025-12-09 14:53:35 +0800

    deleted footer

--- components/InfoFloater.tsx
+++ components/InfoFloater.tsx
@@ -189,9 +189,7 @@ const InfoFloater: React.FC = () => {
                   ))}
                 </div>
                 
-                <div className="text-[10px] text-slate-400 text-center pt-2">
-                    Made with 🤍 Forever App &copy; 2025 by July
-                </div>
+                {/*Deleted footer*/}
               </div>
             )}
 
